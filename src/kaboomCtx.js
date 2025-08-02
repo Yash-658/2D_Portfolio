0a1c4bd7bf85308c9b24c94d08cdd4944936453c
Yash Raghuwanshi <136227872+darkie198@users.noreply.github.com>
2025-08-02 18:40:07 +0530

    .

---
+++ src/kaboomCtx.js
@@ -0,0 +1,7 @@
+import kaboom from "kaboom";
+
+export const k = kaboom({                       // so we gonna call all the fxns we need from this k object, just for clean code, doing this so we don't have to import kaboom globally
+    global: false,                              // this line helps us to implement the above comment.
+    touchToMouse: true,                         // translate all touch events from the phone to mouse click events
+    canvas: document.getElementById("game"),    // passing the html canvas we declared in the index.html to kaboom so that it can take control of it 
+});
