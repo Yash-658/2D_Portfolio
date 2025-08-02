8ea48b318fc2ae1e863e4face7f2e33d8188ad8b
Yash Raghuwanshi <136227872+darkie198@users.noreply.github.com>
2025-08-02 20:44:24 +0530

    .

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ k.loadSprite("spritesheet", "./spritesheet.png", {        // okay so cuz of vite
     sliceX: 39,
     sliceY: 31,
     anims: {
-        "idle-down": 936,
+        "idle-down": 948,
+        "walk-down": {from: 948, to: 951, loop: true, speed: 8},
+        "idle-side": 987,
+        "walk-side": {from: 987, to: 990, loop: true, speed: 8},
+        "idle-up": 1026,
+        "walk-up": {from: 1026, to: 1029, loop: true, speed: 8}
     }
-})
+})  
