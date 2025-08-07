983fccc457f2ffc3cf8fe692d22ddf3dde00ea37
Yash Raghuwanshi <136227872+darkie198@users.noreply.github.com>
2025-08-07 13:28:43 +0530

    .

--- src/constants.js
+++ src/constants.js
@@ -11,7 +11,7 @@ export const dialogueData = {
     tv: `That's my TV. Here I binge watch shows day and night, Some of my favs are:
      Three Body Problems, Stranger Things, Wednesday 🤺`,
     bed: `This is where I sleep. Great ideas comes when I'm lying on my bed... <br>but mostly ideas about how I could have implemented a certain DSA question Differently `,
-    resume: `This is my desk and on it is my resume !!!. <a href="https://github.com/Yash-658/Yash-658/blob/ffe85399cf3aa274d35a9b4e43b9338608021f14/REAL%20RESUME.pdf" target="_blank">Check it out?</a>  
+    resume: `This is my desk and on it is my resume !!!. <br><a href="https://github.com/Yash-658/Yash-658/blob/ffe85399cf3aa274d35a9b4e43b9338608021f14/REAL%20RESUME.pdf" target="_blank">Check it out?</a>  
     <br> Contact me at yashraghuwanshi658@gmail.com if you have any interesting job opportunities!`,
     projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
     Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
