92e1e1beb2d920be57e2e36e96adf8f42b4e6744
Yash Raghuwanshi <136227872+darkie198@users.noreply.github.com>
2025-08-07 11:17:43 +0530

    .

--- src/constants.js
+++ src/constants.js
@@ -5,14 +5,14 @@ export const scaleFactor = 4
 export const dialogueData = {
     pc: `This is my PC, I love banging my head on Leetcode hard prblms in my spare time :)
         I hv also made some android apps using Kotlin and JetPack Compose, Anyway I just like programming smh.
-        Here is my <a href="https://github.com/darkie198" target="_blank">Github</a>!`,
+        Here is my <a href="https://github.com/Yash-658" target="_blank">Github</a>, I will be posting all of my upcoming future projects here!`,
     "cs-degree": `This is a space for my CS degree. I will soon hung it on the wall after graduating in 2027, It's a very theoretical degree (..aha 🫠) but I think it gave me a good foundation.`,
     "sofa-table": `That's my sofa. I like to relax here and watch videos on how to get rich!.`,
     tv: `That's my TV. Here I binge watch shows day and night, Some of my favs are:
      Three Body Problems, Stranger Things, Wednesday 🤺`,
-    bed: `This where I sleep. Great ideas comes when I'm lying on my bed, ...but mostly ideas about how I could have implemented a certain DSA question Differently `,
-    resume: `This is my desk and on it is my resume FIX THIS!!!. <a href="public/Resume.pdf" target="_blank">Check it out?</a>  
-    Contact me at yashraghuwanshi658@gmail.com if you have any interesting job opportunities!`,
+    bed: `This where I sleep. Great ideas comes when I'm lying on my bed... <br>but mostly ideas about how I could have implemented a certain DSA question Differently `,
+    resume: `This is my desk and on it is my resume !!!. <a href="https://github.com/Yash-658/Yash-658/blob/ffe85399cf3aa274d35a9b4e43b9338608021f14/REAL%20RESUME.pdf" target="_blank">Check it out?</a>  
+    <br> Contact me at yashraghuwanshi658@gmail.com if you have any interesting job opportunities!`,
     projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
     Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
     Kaboom.js by watching some of these tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
